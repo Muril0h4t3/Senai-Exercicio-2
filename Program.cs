e6f863f9de0c84f01a6e2aabee4c98be17ad206b
Murilo Dos Santos Gregorio <50981737870@info.local>
2018-09-27 09:07:13 -0300

    mensagem

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace senai.operadores.ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o tamanho da base");
+            float lado = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o comprimento");
+            float comprimento = float.Parse(Console.ReadLine());
+
+            float area = (float)(lado * comprimento); 
+
+            Console.WriteLine("A área é: " + area );
+        }
+    }
+}
